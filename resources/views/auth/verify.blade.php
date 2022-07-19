afe0d96cbecde2a51ee96663019cb0ef23f4644b
Lucien ANDRIAMIHAJA <lucien.peterson@gmail.com>
2022-07-19 08:54:52 +0300

    commit

---
+++ resources/views/auth/verify.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Verifier votre addresse mail') }}</div>
+
+                <div class="card-body">
+                    @if (session('resent'))
+                        <div class="alert alert-success" role="alert">
+                            {{ __('Un nouveau lien de vérification a été envoyé à votre adresse électronique..') }}
+                        </div>
+                    @endif
+
+                    {{ __('Avant de poursuivre, veuillez vérifier votre e-mail pour un lien de vérification..') }}
+                    {{ __('Si vous n'avez pas reçu émail') }},
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
